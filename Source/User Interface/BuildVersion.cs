8b01d99663c400367979fbfad25d6a21ac25cc6e
Wiley Black <TheWiley@gmail.com>
2020-07-13 10:20:30 -0700

    Added some 64-bit dependency DLLs to Release folder.

--- Source/User Interface/BuildVersion.cs
+++ Source/User Interface/BuildVersion.cs
@@ -1,5 +1,5 @@
 using System;
 
 namespace ZippyBackup.User_Interface {
-	public class BuildVersion { public static DateTime Version = DateTime.Parse("2020-07-13T17:06:57.6541206Z"); }
+	public class BuildVersion { public static DateTime Version = DateTime.Parse("2020-07-13T17:15:06.4956352Z"); }
 }
